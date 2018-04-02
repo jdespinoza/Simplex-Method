23c201aa5860a31d6054618fc5253ba330b97fef
frivera12 <friveracr@gmail.com>
2018-04-02 11:06:43 -0600

    Update simplex.py

--- simplex.py
+++ simplex.py
@@ -25,9 +25,11 @@ def open_file(file_name):
 	return aux
 
 def file_to_matrix(line, type):
+	flag_igual = 0
+	largo = 0
 	decision = int(line[4])
 	restrictions = int(line[6])
-	matrix = np.zeros((restrictions+1, restrictions+decision+1))
+	# matrix = np.zeros((restrictions+1, restrictions+decision+1))
 	line = line[8:]
 	line = line.split(',')
 	line_aux = []
@@ -47,26 +49,75 @@ def file_to_matrix(line, type):
 	k = 1
 	flag = -1
 	sign = []
-	for i in range(restrictions+1):
+	cantidad = 0
+	for i in line_aux:
+		if i == '>=':
+			flag_igual=1
+			sign.append(i)
+			cantidad+=1
+		elif i == '<=' or i == '=':
+			sign.append(i)
+	if flag_igual == 1:
+		largo = restrictions+decision+((cantidad)*2)
+	else:
+		largo = restrictions+decision+1
+	print(largo)
+	matrix = np.zeros((restrictions+1, largo))
+	
+	# for p in range(largo):
+	# if p < decision:
+	# matrix[0][p] = line_aux[l]
+	# print(matrix[0][p])
+	# l += 1
+	# else:
+	# matrix[0][p] = 0
+	# print(matrix[0][p])
+
+	for i in range(restrictions+1):	
 		flag += 1
 		k = 1
-		for j in range(restrictions+decision+1):
+		print("algo2")
+		for j in range(largo):
+			print("algo")			
 			if j < decision:
 				matrix[i][j] = line_aux[l]
 				l += 1
 			elif i == 0:
-				matrix[i][j] = 0
+				matrix[0][j] = 0
 			elif j == restrictions+decision:
 				matrix[i][j] = line_aux[l]
 				l += 1
-				sign.append(line_aux[l])
+				# sign.(line_aux[l])
 				l += 1
 			else:
 				if k == flag:
-					matrix[i][j] = 1
+					if flag_igual ==1:
+						matriz[i][j] = -1
+						matriz[i][j+1] = 1
+					else:
+						matrix[i][j] = 1
 				k += 1
+				
+	print(sign)
+	M = 2		
+	for i in range(len(sign)):
+		# print(i)
+		j = (i+decision)
+		print(j)
+		if sign[i] == '=' or sign[i] == '>=':
+			if type ==2:
+				matrix[0][j] = 1*M
+	
+	for i in range(len(sign)):
+		# print(i)
+		if sign[i] == '=' or sign[i] == '>=':
+			for j in range(largo):
+				if type ==2:
+					matrix[0][j] -= matrix[i+1][j]*M
+	# print(sign)
+	print(matrix)
 
-	return SimplexMethod(matrix, restrictions+1, restrictions+decision+1, decision, restrictions, sign)
+	# return SimplexMethod(matrix, restrictions+1,largo, decision, restrictions, sign)
 
 if __name__ == '__main__':
 
@@ -81,10 +132,13 @@ if __name__ == '__main__':
 		if simplex is None:
 			print("Error: Simplex is None")
 		else:
-			simplex.build_matrix()
-			simplex.simplex(args.output)
-			simplex.print_matrix()
-			simplex.print_result()
-
-	if args.output:
-		print("Output File:", args.output)
+			if args.output:
+				simplex.build_matrix()
+				simplex.simplex(args.output)
+				simplex.print_matrix()
+				simplex.print_result()
+			else:
+				simplex.build_matrix()
+				simplex.simplex("salida.txt")
+				simplex.print_matrix()
+				simplex.print_result()
